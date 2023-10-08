ad99d389e02c556ab391df21dc130e985a90ec92
Developer-DineshKumar <kumar05dinesh@gmail.com>
2023-10-08 10:29:31 +0530

    Bank-Passbook-details

--- backend/index.js
+++ backend/index.js
@@ -87,7 +87,13 @@ app.delete("/delete/:id", async (req, res) => {
   res.send({ success: true, message: "data delete successfully", data: data });
 });
 
-mongoose.connect("mongodb://127.0.0.1:27017/bank-passbooks", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/bank-passbooks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to mongodb");
+    app.listen(PORT, () => console.log("server is running"));
+  })
+  .catch((err) => console.log(err));
