e92ba90bf4c67bf9e76f7ddd500beb5dd02be8f2
Developer-DineshKumar <kumar05dinesh@gmail.com>
2023-10-09 10:21:08 +0530

    Bank-Passbook-details

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
 app.use(cors());
@@ -88,7 +89,7 @@ app.delete("/delete/:id", async (req, res) => {
 });
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/bank-passbooks", {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
